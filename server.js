11b6892d57a9e9b8c1e93e84a5ff2782b4696027
ramy sabry <ramyalsabry.dev@gmail.com>
2025-02-13 13:57:15 +0200

    configuration vercel

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-const expenses = [];
+
+if (!process.env.MONGO_CONNECTION) {
+  console.error("Error: MONGO_CONNECTION environment variable is missing.");
+  process.exit(1);
+}
 
 app.use(express.json());
 
@@ -21,7 +27,7 @@ const expenseSchema = new mongoose.Schema({
 const Expense = mongoose.model("Expense", expenseSchema);
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // GET ALL EXPENSES DATA
@@ -30,12 +36,70 @@ app.get("/api/expenses", async (req, res) => {
   res.json(expenses);
 });
 
-// CREATE A NEW EXPENSES
+// CREATE A NEW EXPENSE
 app.post("/api/expenses", async (req, res) => {
-  const expense = new Expense(req.body);
-  await expense.save();
-
-  res
-    .status(201, { message: "the expense was created successfully!" })
-    .json(expense);
+  try {
+    const expense = new Expense(req.body);
+    await expense.save();
+    res.status(201).json(expense);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
+
+// GET ONE EXPENSE
+app.get("/api/expenses/:id", async (req, res) => {
+  const expense = await Expense.findById(req.params.id);
+  if (!expense) {
+    return res.status(404).json({ error: "Expense not found" });
+  }
+
+  res.status(200).json(expense);
+});
+
+// UPDATE EXPENSE
+app.put("/api/expenses/:id", async (req, res) => {
+  const updatedExpense = await Expense.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedExpense) {
+    return res.status(404).json({ error: "Expense not fount" });
+  }
+
+  res.status(200).json(updatedExpense);
+});
+
+// DELETE EXPENSE
+app.delete("/api/expenses/:id", async (req, res) => {
+  const targetExpense = await Expense.findByIdAndDelete(req.params.id);
+
+  if (!targetExpense) {
+    return res.status(404).json({ error: "Expense not found" });
+  }
+
+  if (targetExpense.length === 0) {
+    return res.status(404).json({ error: "Expense not found" });
+  }
+
+  res.status(200).json({
+    message: "The expense was deleted successfully!",
+    deletedExpense: targetExpense,
+  });
+});
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Backend server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("🚀 ~ mongoose.connect ~ err:", err);
+  });
